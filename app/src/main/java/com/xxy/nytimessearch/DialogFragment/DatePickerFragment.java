4b4112611a6f45285c9ac82af9a83af35a7d8eeb
Xiangyang_Xiao <Xiangyang.Xiao@airbnb.com>
2016-02-14 21:38:41 -0800

    [nytimessearch] Add readme and walkthrough

---
+++ app/src/main/java/com/xxy/nytimessearch/DialogFragment/DatePickerFragment.java
@@ -0,0 +1,34 @@
+package com.xxy.nytimessearch.DialogFragment;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+import com.xxy.nytimessearch.Listener.DatePickerListener;
+
+/**
+ * Created by xiangyang_xiao on 2/14/16.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+
+  private DatePickerListener mListener;
+
+  public void setListener(
+      DatePickerListener listener
+  ) {
+    mListener = listener;
+  }
+
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    // Create a new instance of TimePickerDialog and return it
+    return new DatePickerDialog(
+        getActivity(),
+        mListener,
+        mListener.getYear(),
+        mListener.getMonth(),
+        mListener.getDay());
+  }
+}
